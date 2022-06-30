e8e07108a4d9e8ae332497d27d745ed47de156f6
AbderrahmaneMouzoune <abderrahmane.mouzoune@gmail.com>
2022-06-30 12:34:29 +0200

    ajout product page

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import React from "react"
 
 export default function Product() {
-    return (
-        <>
-            Hello Product
-        </>
-    )
+  return (
+    <div className="product-container">
+      <figure className="image-container">
+        <img src="https://wallpaperaccess.com/full/1354642.jpg" alt="Voiture" />
+        <div className="back-button">
+          <span className="material-symbols-rounded">arrow_back_ios</span>
+        </div>
+        <div className="fav-button">
+          <span className="material-symbols-rounded">favorite</span>
+        </div>
+        <div className="dot-container">
+          <span className="dot active"></span>
+          <span className="dot"></span>
+          <span className="dot"></span>
+        </div>
+      </figure>
+      <div className="product-wrapper-desc">
+        <div className="product-title">
+          <h2>OPEL CORSA-E</h2>
+          <div>
+            <span className="material-symbols-rounded">star</span>4,6/5
+          </div>
+        </div>
+
+        <div className="product-description">
+          <h2>Description</h2>
+          <div className="product-description-wrapper">
+            <div className="product-description-content">
+              <p>0 - 100 km/h</p>
+              <p className="product-green">8,1 sec</p>
+            </div>
+            <div className="product-description-content">
+              <p>Autonomie</p>
+              <p className="product-green">550km</p>
+            </div>
+            <div className="product-description-content">
+              <p>Recharge max</p>
+              <p className="product-green">100KW</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="product-caracteristique">
+          <div className="product-caracteristique-wrapper">
+            <p>
+              <b>Récupération</b>
+            </p>
+            <p>Gare Paris 11ème</p>
+          </div>
+          <div className="product-caracteristique-wrapper">
+            <p>
+              <b>Dépôt</b>
+            </p>
+            <p>Parking Gare</p>
+          </div>
+        </div>
+
+        <div className="product-reservation">
+          <div className="product-price">44€/ jour</div>
+          <div className="product-price product-cta">Réserver maintenant</div>
+        </div>
+      </div>
+    </div>
+  )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -6,20 +6,21 @@ import ProductLayout from "./pages/ProductLayout"
 import Home from "./pages/Home"
 import CalendarLayout from "./components/CalendarLayout"
 import ProductSerp from "./components/ProductSerp"
+import Product from "./components/Product"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}> 
+        <Route path="/" element={<Home />}></Route>
+        <Route path="calendar" element={<ProductLayout />}>
+          <Route path=":idCalendar" element={<CalendarLayout />} />
         </Route>
-        <Route path="calendar" element={<ProductLayout />}> 
-          <Route path=":idCalendar" element={<CalendarLayout />} /> 
-        </Route> 
         <Route path="product" element={<ProductLayout />}>
-          <Route path=":idProduit" element={<ProductSerp />} /> 
-        </Route> 
+          <Route path=":idProduit" element={<ProductSerp />} />
+        </Route>
+        <Route path="productDesc" element={<Product />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
